6e5f1fbba75e7b322d12a5c6d7f416da8a3a0d60
Sam <1631095+takuy@users.noreply.github.com>
2019-01-02 12:56:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if "OPT" in cfg_vars:
     cfg_vars["OPT"] = cfg_vars["OPT"].replace("-Wstrict-prototypes", "")
 
 python_cec = Extension('cec', sources = [ 'cec.cpp', 'device.cpp' ], 
-                        include_dirs=['include'],
+                        include_dirs=['include', 'C:\Program Files (x86)\Pulse-Eight\USB-CEC Adapter'],
                         libraries = [ ])
 
 setup(name='cec', version='0.2.7',
